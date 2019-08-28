c8195f63ee50230acf3ba88275fe1e4942dc612d
Antony Papadimitriou <ant.papa22@gmail.com>
2019-08-28 23:50:06 +1000

    add message to slack channel

--- prediction/run_predictions.py
+++ prediction/run_predictions.py
@@ -9,144 +9,156 @@ from sqlalchemy import create_engine
 import warnings
 warnings.filterwarnings("ignore")
 
+try:
+    # initialise h2o
+    h2o.init()
 
-# initialise h2o
-h2o.init()
+    # import postcode data
+    print("==============================================")
+    print("Importing Postcode Data")
+    print("==============================================")
+    postcode_data = pd.read_csv("australian_postcodes.csv")
 
-# import postcode data
-print("==============================================")
-print("Importing Postcode Data")
-print("==============================================")
-postcode_data = pd.read_csv("australian_postcodes.csv")
+    # filter for vic postcodes
+    vic_postcodes = postcode_data[postcode_data["State"] == "VIC"]
+    vic_postcodes = vic_postcodes.drop_duplicates(subset="postcode")
+    vic_postcodes.head()
 
-# filter for vic postcodes
-vic_postcodes = postcode_data[postcode_data["State"] == "VIC"]
-vic_postcodes = vic_postcodes.drop_duplicates(subset="postcode")
-vic_postcodes.head()
+    # create postcode data
+    postcode_list = []
 
-# create postcode data
-postcode_list = []
+    for index, row in vic_postcodes.iterrows():
+        if np.isnan(row["lat"]):
+            postcode_dict = dict()
+            postcode_dict["postcode"] = row["postcode"]
+            postcode_dict["coordinates"] = [-37.814, 144.9632]
+            postcode_list.append(postcode_dict)
+        else:
+            postcode_dict = dict()
+            postcode_dict["postcode"] = row["postcode"]
+            postcode_dict["coordinates"] = [row["lat"], row["long"]]
+            postcode_list.append(postcode_dict)
 
-for index, row in vic_postcodes.iterrows():
-    if np.isnan(row["lat"]):
-        postcode_dict = dict()
-        postcode_dict["postcode"] = row["postcode"]
-        postcode_dict["coordinates"] = [-37.814, 144.9632]
-        postcode_list.append(postcode_dict)
-    else:
-        postcode_dict = dict()
-        postcode_dict["postcode"] = row["postcode"]
-        postcode_dict["coordinates"] = [row["lat"], row["long"]]
-        postcode_list.append(postcode_dict)
+    # get weather data for every postcode
 
-# get weather data for every postcode
+    # set api key for dark sky weather data
+    api_key = os.environ["WEATHER_API_KEY"]
 
-# set api key for dark sky weather data
-api_key = os.environ["WEATHER_API_KEY"]
+    # store weather data
+    weather_data = []
 
-# store weather data
-weather_data = []
+    # loop through postcodes and get weather data
+    print("==============================================")
+    print("Importing Weather Data")
+    print("==============================================")
+    for postcode in postcode_list:
+        lat = str(postcode["coordinates"][0])
+        lon = str(postcode["coordinates"][1])
+        suffix = lat + "," + lon + "?exclude=currently,minutely,hourly,alerts,flags"
+        url = "https://api.darksky.net/forecast/" + api_key + "/" + suffix
+        response = requests.get(url)
+        weather_json = json.loads(response.text)
+        weather_dict = dict()
+        weather_dict["postcode"] = postcode["postcode"]
+        weather_dict["data"] = weather_json
+        weather_dict["latitude"] = postcode["coordinates"][0]
+        weather_dict["longitude"] = postcode["coordinates"][1]
+        weather_data.append(weather_dict)
 
-# loop through postcodes and get weather data
-print("==============================================")
-print("Importing Weather Data")
-print("==============================================")
-for postcode in postcode_list:
-    lat = str(postcode["coordinates"][0])
-    lon = str(postcode["coordinates"][1])
-    suffix = lat + "," + lon + "?exclude=currently,minutely,hourly,alerts,flags"
-    url = "https://api.darksky.net/forecast/" + api_key + "/" + suffix
-    response = requests.get(url)
-    weather_json = json.loads(response.text)
-    weather_dict = dict()
-    weather_dict["postcode"] = postcode["postcode"]
-    weather_dict["data"] = weather_json
-    weather_dict["latitude"] = postcode["coordinates"][0]
-    weather_dict["longitude"] = postcode["coordinates"][1]
-    weather_data.append(weather_dict)
+    # get postcode weather data in df and then convert to h2o frame
+    overall_df = pd.DataFrame()
 
-# get postcode weather data in df and then convert to h2o frame
-overall_df = pd.DataFrame()
+    for point in weather_data:
+        try:
+            df = pd.DataFrame()
+            df = df.append(point["data"]["daily"]["data"][0], ignore_index=True)
+            df["postcode"] = point["postcode"]
+            df["latitude"] = point["latitude"]
+            df["longitude"] = point["longitude"]
+            overall_df = overall_df.append(df, ignore_index=True)
+        except:
+            continue
 
-for point in weather_data:
-    try:
-        df = pd.DataFrame()
-        df = df.append(point["data"]["daily"]["data"][0], ignore_index=True)
-        df["postcode"] = point["postcode"]
-        df["latitude"] = point["latitude"]
-        df["longitude"] = point["longitude"]
-        overall_df = overall_df.append(df, ignore_index=True)
-    except:
-        continue
+    new_data = h2o.H2OFrame(overall_df)
 
-new_data = h2o.H2OFrame(overall_df)
+    # load model
+    model = h2o.load_model("gbm_grid2_model_28")
 
-# load model
-model = h2o.load_model("gbm_grid2_model_28")
+    # generate predictions
+    print("==============================================")
+    print("Generating Predictions")
+    print("==============================================")
+    predictions = model.predict(new_data)
 
-# generate predictions
-print("==============================================")
-print("Generating Predictions")
-print("==============================================")
-predictions = model.predict(new_data)
+    # append predictions to df
+    overall_df["prediction"] = predictions.as_data_frame()
 
-# append predictions to df
-overall_df["prediction"] = predictions.as_data_frame()
+    # get necessary columns and edit dataframe
+    prediction_df = overall_df[["prediction", "temperatureHigh", "pressure", "humidity", "windSpeed", "postcode"]].copy()
+    prediction_df.rename(columns={"prediction": "bushfireProb",
+                                  "temperatureHigh": "airTemperature",
+                                  "pressure": "airPressure",
+                                  "humidity": "humidity",
+                                  "windSpeed": "windSpeed"}, inplace=True)
 
-# get necessary columns and edit dataframe
-prediction_df = overall_df[["prediction", "temperatureHigh", "pressure", "humidity", "windSpeed", "postcode"]].copy()
-prediction_df.rename(columns={"prediction": "bushfireProb",
-                              "temperatureHigh": "airTemperature",
-                              "pressure": "airPressure",
-                              "humidity": "humidity",
-                              "windSpeed": "windSpeed"}, inplace=True)
+    # convert fahrenheit to celsius
+    prediction_df["airTemperature"] = prediction_df["airTemperature"].apply(lambda x: (x - 32) * 5.0 / 9.0)
+    prediction_df["airTemperature"] = prediction_df["airTemperature"].astype("int64")
 
-# convert fahrenheit to celsius
-prediction_df["airTemperature"] = prediction_df["airTemperature"].apply(lambda x: (x - 32) * 5.0 / 9.0)
-prediction_df["airTemperature"] = prediction_df["airTemperature"].astype("int64")
+    # convert miles per hour to km per hour
+    prediction_df["windSpeed"] = prediction_df["windSpeed"].apply(lambda x: x / 0.62137)
+    prediction_df["windSpeed"] = prediction_df["windSpeed"].astype("int64")
 
-# convert miles per hour to km per hour
-prediction_df["windSpeed"] = prediction_df["windSpeed"].apply(lambda x: x / 0.62137)
-prediction_df["windSpeed"] = prediction_df["windSpeed"].astype("int64")
-
-# convert humidity to percentage
-prediction_df["humidity"] = prediction_df["humidity"] * 100
-prediction_df["humidity"] = prediction_df["humidity"].astype("int64")
-
-
-# create bushfire risk and severity rating
-def bushfire_risk_rating(prob):
-    if prob <= 0.33:
-        risk_rating = "LOW"
-    elif (prob > 0.33) & (prob <= 0.66):
-        risk_rating = "MEDIUM"
-    else:
-        risk_rating = "HIGH"
-    return risk_rating
+    # convert humidity to percentage
+    prediction_df["humidity"] = prediction_df["humidity"] * 100
+    prediction_df["humidity"] = prediction_df["humidity"].astype("int64")
 
 
-def bushfire_severity_rating(prob):
-    if prob <= 0.33:
-        severity_rating = "MINIMAL"
-    elif (prob > 0.33) & (prob <= 0.66):
-        severity_rating = "MEDIUM"
-    else:
-        severity_rating = "STRONG"
-    return severity_rating
+    # create bushfire risk and severity rating
+    def bushfire_risk_rating(prob):
+        if prob <= 0.33:
+            risk_rating = "LOW"
+        elif (prob > 0.33) & (prob <= 0.66):
+            risk_rating = "MEDIUM"
+        else:
+            risk_rating = "HIGH"
+        return risk_rating
 
 
-prediction_df["bushfireRiskRating"] = prediction_df["bushfireProb"].map(bushfire_risk_rating)
-prediction_df["bushfireSeverityRating"] = prediction_df["bushfireProb"].map(bushfire_severity_rating)
+    def bushfire_severity_rating(prob):
+        if prob <= 0.33:
+            severity_rating = "MINIMAL"
+        elif (prob > 0.33) & (prob <= 0.66):
+            severity_rating = "MEDIUM"
+        else:
+            severity_rating = "STRONG"
+        return severity_rating
 
-# add timestamp
-prediction_df["lastUpdated"] = datetime.now()
 
-# write to db
-print("==============================================")
-print("Writing to Database")
-print("==============================================")
-engine = create_engine(os.environ["CC_DB_PATH"])
-prediction_df.to_sql(con=engine, name="predictions", if_exists="replace", index=False)
+    prediction_df["bushfireRiskRating"] = prediction_df["bushfireProb"].map(bushfire_risk_rating)
+    prediction_df["bushfireSeverityRating"] = prediction_df["bushfireProb"].map(bushfire_severity_rating)
 
-# close h2o cluster
-h2o.cluster().shutdown()
+    # add timestamp
+    prediction_df["lastUpdated"] = datetime.now()
+
+    # write to db
+    print("==============================================")
+    print("Writing to Database")
+    print("==============================================")
+    engine = create_engine(os.environ["CC_DB_PATH"])
+    prediction_df.to_sql(con=engine, name="predictions", if_exists="replace", index=False)
+
+    # close h2o cluster
+    h2o.cluster().shutdown()
+
+    # send message to slack channel
+    error_message = "Bushfire Prediction Model Run"
+    slackurl = os.environ["SLACK_HOOK"]
+    slackpayload = {'text': error_message}
+    response = requests.post(slackurl, data=json.dumps(slackpayload), headers={'Content-Type': 'application/json'})
+except:
+    # send error to slack channel
+    error_message = "Bushfire Prediction Model Failed"
+    slackurl = os.environ["SLACK_HOOK"]
+    slackpayload = {'text': error_message}
+    response = requests.post(slackurl, data=json.dumps(slackpayload), headers={'Content-Type': 'application/json'})
